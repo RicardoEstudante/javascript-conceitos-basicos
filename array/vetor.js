1c76bee3afd221e79cec880f512c0b24d85380f0
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-02-19 17:13:28 -0300

    Adicionando mais scripts

---
+++ array/vetor.js
@@ -0,0 +1,13 @@
+- tipo especial de objeto
+- conjunto ordenado de valores numerados por um indice (começa em 0);
+
+var array = new Array();
+
+array[0] = "primeiro indice";
+array[1] = "segundo indice";
+array[2] = "terceiro indice";
+
+console.log(array.length); //Deve retornar o valor 3! (numero do maior indice +1);
+
+var array2 = ["0", "1", "3", "4"];
+console.log(array2.length); //deve retornar 4;  
