05448eff02c4b3f6c9293349d405fcc1585869b6
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-02-21 11:58:26 -0300

    add erros

---
+++ status-code/erros.js
@@ -0,0 +1,28 @@
+// Respostas de informação (100-199),
+// # 100 Continue
+// # 101 Switching Protocol
+// # 103 Early Hints 
+
+// Respostas de sucesso (200-299),
+// # 200 ok
+// # 201 Created
+// # 204 no Content
+// # 205 Reset Content
+
+// Redirecionamentos (300-399),
+// # 300 Multiple Choice
+// # 302 Found
+// # 303 See Other
+// # 304 Not Modified
+
+// Erros do cliente (400-499)
+// # 400 Bad Request
+// # 401 Unauthorized
+// # 403 Forbidden
+// # 404 Not Found
+
+// Erros do servidor (500-599)
+// # 500 Internal Server Error
+// # 501 Not Implemented
+// # 502 Bad Gateway
+// # 510 Not Extended
