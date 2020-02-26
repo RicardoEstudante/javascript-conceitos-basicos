8ce5c7a1fb9101ecc5dc246f3c68d289a2e70cb4
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-02-26 15:21:46 -0300

    slice()

--- Strings/splice/splice.js
+++ Strings/splice/splice.js
@@ -16,12 +16,15 @@ var resultado2 = nome.slice(4, nome.length);
 var nome = ["Ricardo", "Carvalho", "Santos", "da", "Silva", "Souza"]
 var resultado3 = nome.slice(4, nome.length-1);
 
+var nomeString = "Ricardo, Carvalho, Santos";
+var resultado4 = nomeString.slice(9)
 
 try{
     assert.deepEqual(["Silva", "Souza"], resultado)
     assert.deepEqual(["Silva", "Souza"], resultado1)
     assert.deepEqual(["Silva", "Souza"], resultado2)
     assert.deepEqual(["Silva"],          resultado3)
+    assert.deepEqual("Carvalho, Santos",          resultado4)
 }catch (e) {
     console.log(e)
 }
