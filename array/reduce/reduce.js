e5ec6eecfe37bcbc1897d77fb404da410f3551b9
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-02-26 17:24:49 -0300

    reduce

--- array/reduce/reduce.js
+++ array/reduce/reduce.js
@@ -5,12 +5,11 @@ var assert = require('assert');
 
 var exemplo = [1,2,3,4,5];
 var total = exemplo.reduce(function (total, num) {
-
         return total + num;
-});
+}); // função para somar todos os membros do array
 
 var exemplo2 = [1,2,3,4];
-var total1 = exemplo2.reduce((total, num) => total + num, 0);
+var total1 = exemplo2.reduce((total, num) => total + num, 0); // arrow function para somar todos os membros do array
 
 try{
     assert.equal(15, total);
