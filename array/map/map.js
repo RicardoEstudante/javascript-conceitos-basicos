7ffbb5bdfdc347d143db59509f1bd04b9512ead8
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-02-26 17:06:32 -0300

    map

---
+++ array/map/map.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+
+// a função map nos permite percorrer um array e retornar um novo array
+// cujos itens são o resultado de uma função de callback
+// que recebe como parametro cada item do array original
+
+
+var teste = [1,2,3];
+var dobro = teste.map(function (item) {
+        return item * 2;
+});
+
+var teste2 = [1,2,3];
+var dobro2 = teste2.map(function (item) {
+        return Math.pow(item, 2);
+});
+
+try{
+    assert.deepEqual([2,4,6], dobro)
+    assert.deepEqual([1,4,9], dobro2)
+}catch (e) {
+    console.log(e)
+}
