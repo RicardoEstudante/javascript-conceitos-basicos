1f7332cffbf56109ff9ca48e18f9309ae42cff5a
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-02-26 17:23:05 -0300

    reduce

---
+++ array/reduce/reduce.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+
+// reduce itera um array e retorna um objeto final
+// de acordo com a logica implementada na função de callback
+
+var exemplo = [1,2,3,4,5];
+var total = exemplo.reduce(function (total, num) {
+
+        return total + num;
+});
+
+var exemplo2 = [1,2,3,4];
+var total1 = exemplo2.reduce((total, num) => total + num, 0);
+
+try{
+    assert.equal(15, total);
+    assert.equal(10, total1);
+}catch (e) {
+    console.log(e);
+}
