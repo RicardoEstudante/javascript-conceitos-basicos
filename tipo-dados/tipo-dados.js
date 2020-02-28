dec6e54c4c9492a488a0695a29e518a599b040a1
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-02-28 13:24:04 -0300

    tipo de dados

--- tipo-dados/tipo-dados.js
+++ tipo-dados/tipo-dados.js
@@ -6,6 +6,10 @@
 * const |  Usado para declarar constantes
  */
 
-var string = "String";
-var number = 10;
-var boolean = true;
+var strings = "String";                 //tipo de dados para textos
+var numbers = 10;                       //tipo de dados para valores númericos inteiros/reais
+var booleans = true;                    //tipo de dados para valores lógicos false/true
+var arrays = ["A","R","R","A","Y"];     //tipo de dados para vetores
+var nulls = null;                       //tipo de dados para valores que não precisa de atribuição
+var undefineds;                         //tipo de dado definido pelo javascript quando uma variavel não é inicializada
+
