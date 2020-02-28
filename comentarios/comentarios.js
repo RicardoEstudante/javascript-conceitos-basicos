74bc1bfc5560c0f82fba3383e0dda80b2feb0969
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-02-28 12:21:01 -0300

    Comentários

---
+++ comentarios/comentarios.js
@@ -0,0 +1,18 @@
+
+console.log("Comentário de uma única linha");
+// comentando em uma única linha
+
+console.log("Comentário de multiplas linhas ");
+/*
+   comentando em
+   várias
+   linha
+ */
+
+console.log("Comentário de documentação");
+
+/*
+* comentando
+* como
+* documentação
+ */
