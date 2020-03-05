42e0b88506f4b62806d2e42a67e8d8ceeffc966e
Ricardo Carvalho <ricardo2010carvalho@hotmail.com>
2020-03-05 14:02:53 -0300

    DOM

---
+++ dom/pegandoElementos.js
@@ -0,0 +1,6 @@
+document.getElementById();             // seleciona um elemento pela ID
+document.getElementsByClassName();     // seleciona um elemento pela class
+document.getElementsByTagName();       // seleciona todos os elementos pela tag
+document.getElementsByTagName()[0];    // seleciona o primeiro elemento pela tag
+document.querySelector();              // seleciona um elemento pelo seletor
+document.querySelectorAll();           // seleciona todos os elementos pelo seletor
