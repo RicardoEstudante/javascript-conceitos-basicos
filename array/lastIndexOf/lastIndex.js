1dac725f8c80dc04eee70cb31d97849c250f0c52
RicardoSantos-100 <ricardo.santos@docly.com.br>
2021-04-19 22:38:00 -0300

    add methods - some, lastIndexOf

---
+++ array/lastIndexOf/lastIndex.js
@@ -0,0 +1,9 @@
+// Retorna o indice do elemento buscado
+// Se o elemento não existir retorna -1
+
+
+const array = [1,2,3,4,5];
+
+const lastIndex = array.lastIndexOf(5);
+
+console.log(lastIndex);
---
+++ array/some/some.js
@@ -0,0 +1,10 @@
+
+// Retorna true ou false se o array contem o valor verificado
+// parametros (value, index, array)
+
+
+const array = [0,1,2,3,4,5];
+
+const some = array.some((value) => value === 7 )
+
+console.log(some);
